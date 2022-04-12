4f3e38b94b4eac9d890b9e6451e9dab19e4c9237
David Cohen <dcohen@pearsonpkg.com>
2022-04-12 17:03:46 -0400

    Started building class structure

---
+++ app/src/main/java/swervemath/Main.java
@@ -0,0 +1,10 @@
+package swervemath;
+
+public class Main {
+    public static void main(String[] args) {
+        Swerve swerve = new Swerve();
+
+        swerve.doSwerve();
+    }
+    
+}
---
+++ app/src/main/java/swervemath/Swerve.java
@@ -0,0 +1,11 @@
+package swervemath;
+
+public class Swerve {
+    Swerve() {
+        System.out.println("Swerving");
+    }
+
+    public void doSwerve() {
+        System.out.println("Do some swerve");
+    }
+}
