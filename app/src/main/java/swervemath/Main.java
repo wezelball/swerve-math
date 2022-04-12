f9741052735155eba813c29be9018c0f06bdef67
David Cohen <dcohen@pearsonpkg.com>
2022-04-12 17:26:50 -0400

    Started adding methods

--- app/src/main/java/swervemath/Main.java
+++ app/src/main/java/swervemath/Main.java
@@ -2,9 +2,13 @@ package swervemath;
 
 public class Main {
     public static void main(String[] args) {
-        Swerve swerve = new Swerve();
+        Swerve swerve = new Swerve(-0.5, 0.25, 0.75, 120);
 
-        swerve.doSwerve();
+        System.out.println("Forward cmd: " + swerve.getForward());
+        System.out.println("Strafe cmd: " + swerve.getStrafe());
+        System.out.println("Forward cmd: " + swerve.getRotation());
+        // Gyro angle in degrees, converted to radians in Swerve
+        System.out.println("Gyro angle: " + swerve.getGyroAngle());
     }
     
 }
--- app/src/main/java/swervemath/Swerve.java
+++ app/src/main/java/swervemath/Swerve.java
@@ -1,11 +1,36 @@
 package swervemath;
 
 public class Swerve {
-    Swerve() {
-        System.out.println("Swerving");
+
+    private double forward = 0.0;
+    private double strafe = 0.0;
+    private double rotation = 0.0;
+    private double gyroAngle = 0.0;
+
+    Swerve(double fwd, double str, double rot, double gyro) {
+        this.forward = fwd;
+        this.strafe = str;
+        this.rotation = rot;
+        this.gyroAngle = gyro * (Math.PI/180);
+
+        System.out.println("Constructor");
     }
 
-    public void doSwerve() {
-        System.out.println("Do some swerve");
+    public double getForward() {
+        return this.forward;
     }
+
+    public double getStrafe() {
+        return this.strafe;
+    }
+
+    public double getRotation() {
+        return this.rotation;
+    }
+
+    public double getGyroAngle() {
+        return this.gyroAngle;
+    }
+
+    
 }
